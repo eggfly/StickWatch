b95994793e5e169e29ae8d4a997eb7f2054120fd
eggfly <lihaohua90@gmail.com>
2019-01-31 21:58:59 +0800

    some demo
    Signed-off-by: eggfly <lihaohua90@gmail.com>

---
+++ arduino_demo/IRsendDemo/IRsendDemo.ino
@@ -0,0 +1,21 @@
+/*
+   IRremote: IRsendDemo - demonstrates sending IR codes with IRsend
+   An IR LED must be connected to Arduino PWM pin 3.
+   Version 0.1 July, 2009
+   Copyright 2009 Ken Shirriff
+   http://arcfn.com
+*/
+
+
+#include <IRremote.h>
+
+IRsend irsend;
+
+void setup()
+{
+}
+
+void loop() {
+  irsend.sendSony(0x6A2E0000, 32);
+  delay(5000); //5 second delay between each signal burst
+}
